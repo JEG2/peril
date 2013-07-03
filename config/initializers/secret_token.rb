733e224acdb666575d26aceb0397496938ccb87b
James Edward Gray II <james@graysoftinc.com>
2013-07-03 09:14:35 -0500

    The initial Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peril::Application.config.secret_key_base = 'c0b5d78d7eecd8207f1ddea404a2c537fadaa03f20bb852322ce5cab0801c14395e7d78adb4341ec269201a700a14d56bd933f4498414b0b6596a06b0e57f60e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Peril::Application.config.session_store :cookie_store, key: '_peril_session'
