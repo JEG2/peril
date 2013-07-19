8fdb5f9063259ffc6f190905e0c314a6d7677a4b
James Edward Gray II <james@graysoftinc.com>
2013-07-19 13:21:07 -0500

    Using the original LSRC 2013 game as the example.

--- games/example.rb
+++ games/example.rb
@@ -1,167 +1,169 @@
-game "Example"
+game "LoneStarRuby Conference"
 
-players "One", "Two", "Three"
+players "Avdi", "Dave", "Steve"
 
 rewards 200, 400, 600, 800, 1_000
 
-category "A"
+category "People & History"
 answer <<END_ANSWER, <<END_QUESTION
-A1
+These were the first two books published about Ruby in English.
 END_ANSWER
-Q1
+What are _Programming Ruby_ and _The Ruby Way_?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A2
+August 19th, 2009.
 END_ANSWER
-Q2
+What day did _Why the Luck Stiff_ withdraw from the Ruby community?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A3
+He holds the "all time" record for Rails commits, ahead of David Heinemeier Hansson, Aaron Patterson, more.
 END_ANSWER
-Q3
+Who is Jeremy Kemper?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A4
+Before his death, he used his knowledge of Ruby's internals to answer, primarily in Ruby, questions on mailing lists.
 END_ANSWER
-Q4
+Who was Guy Decoux?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A5
+This name was rejected, in favor of "Ruby," during a chat session between Yukihiro Matsumoto and Keiju Ishitsuka in 1993.
 END_ANSWER
-Q5
-END_QUESTION
-
-category "B"
-answer <<END_ANSWER, <<END_QUESTION
-A1
-END_ANSWER
-Q1
-END_QUESTION
-answer <<END_ANSWER, <<END_QUESTION
-A2
-END_ANSWER
-Q2
-END_QUESTION
-answer <<END_ANSWER, <<END_QUESTION
-A3
-END_ANSWER
-Q3
-END_QUESTION
-answer <<END_ANSWER, <<END_QUESTION
-A4
-END_ANSWER
-Q4
-END_QUESTION
-answer <<END_ANSWER, <<END_QUESTION
-A5
-END_ANSWER
-Q5
+What is "Coral?"
 END_QUESTION
 
-category "C"
+category "Syntax"
 answer <<END_ANSWER, <<END_QUESTION
-A1
+The company named after this operator will be pleased to hear that the rumors of its death in Ruby 1.9 are exaggerated.
 END_ANSWER
-Q1
+What is the "Hashrocket" (`=>`)?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A2
+This operator was added to support method-like parameters, but it stayed in Ruby even after block parameters were enhanced.
 END_ANSWER
-Q2
+What is the Arrow Operator or "Stabby Lambda" (`->() { }`)?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A3
+This abuse of another operator is generally relegated to scripting hacks that involve quick and dirty filtering of data.
 END_ANSWER
-Q3
+What is the "Flip-flop" Operator (`..` or `...`)?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A4
+These expressions have an implicit `.to_s` tacked onto their result.
 END_ANSWER
-Q4
+What is interpolation (`"\#{}"`)?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A5
+This character is the only way to define a parameter list that accepts multiple arguments but sets no local variables.
 END_ANSWER
-Q5
-END_QUESTION
-
-category "D"
-answer <<END_ANSWER, <<END_QUESTION
-A1
-END_ANSWER
-Q1
-END_QUESTION
-answer <<END_ANSWER, <<END_QUESTION
-A2
-END_ANSWER
-Q2
-END_QUESTION
-answer <<END_ANSWER, <<END_QUESTION
-A3
-END_ANSWER
-Q3
-END_QUESTION
-answer <<END_ANSWER, <<END_QUESTION
-A4
-END_ANSWER
-Q4
-END_QUESTION
-answer <<END_ANSWER, <<END_QUESTION
-A5
-END_ANSWER
-Q5
+What is the asterisk or "Naked Splat" (`*`)?
 END_QUESTION
 
-category "E"
+category "Core Methods"
 answer <<END_ANSWER, <<END_QUESTION
-A1
+This mix-in isn't as popular as `Enumerable`, but it does add several methods for ordering objects.
 END_ANSWER
-Q1
+What is `Comparable`?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A2
+This iterator was added as an alternative to `inject()` when the same object is always returned from the block.
 END_ANSWER
-Q2
+What is#{' ' * 2}
+`Enumerable#
+each_with_object()`?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A3
+This method flips some common iterators from greedy to conservative allowing for the construction of infinite data pipelines.
 END_ANSWER
-Q3
+What is `Enumerable#lazy()`?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A4
+This method, recently added to `Array` and `Range`, has the potential to speed up some very large data traversals.
 END_ANSWER
-Q4
+What is `bsearch()`?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A5
+This method removes the need to roll you own or load `WEBrick` just to steal a bit of code, both common in earlier days.
 END_ANSWER
-Q5
+What is `Process.daemon()`?
 END_QUESTION
 
-category "F"
+category "Standard Library"
 answer <<END_ANSWER, <<END_QUESTION
-A1
+This library gives Ruby's cryptic global variables much more readable names.
 END_ANSWER
-Q1
+What is `English`?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A2
+This library can be used to generate random bytes, strings of hexidecimal characters, UUID's, and more.
 END_ANSWER
-Q2
+What is `SecureRandom`?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A3
+This library gave user level access to Ruby's parser.
 END_ANSWER
-Q3
+What is `Ripper`?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A4
+It got considerably easier to get the size of the terminal Ruby is running in when this standard library was added.
 END_ANSWER
-Q4
+What is `io/console`?
 END_QUESTION
 answer <<END_ANSWER, <<END_QUESTION
-A5
+Though not its primary purpose, this library gives us a great one-liner for launching a Web server.
 END_ANSWER
-Q5
+What is `un`?
+END_QUESTION
+
+category "Rails"
+answer <<END_ANSWER, <<END_QUESTION
+This is the only command you don't need to prefix with `bundle exec`.
+END_ANSWER
+What is `rails`?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+If you had a JavaScript event trigger in your application that is misbehaving after the 4.0 upgrade, this new feature may be to blame.
+END_ANSWER
+What is `turbolinks`?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This routing feature can restrict matches to requests on a specified subdomain or using a given protocol.
+END_ANSWER
+What are "constraints?"
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This no-longer-documented automation will reverse then reapply the last set of database schema changes.
+END_ANSWER
+What is `rake db:migrate:redo`?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This rarely seen flag to the association macros gives Rails the hint to reuse an in-memory Ruby instance.
+END_ANSWER
+What is `:inverse_of`?
+END_QUESTION
+
+category "Off-topic"
+answer <<END_ANSWER, <<END_QUESTION
+It dictates that there can be no interference with the internal development of alien civilizations.
+END_ANSWER
+What is "The Prime Directive?"
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+Though their motto is "Live Music Capital of the World," this city hosts a yearly Ruby conference.
+END_ANSWER
+What is Austin, TX?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This is the technique of partially cooking food so that it can be finished later.
+END_ANSWER
+What is par-cooking?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+Humans share about 60% of their DNA with this common food item.
+END_ANSWER
+What is a banana?
+END_QUESTION
+answer <<END_ANSWER, <<END_QUESTION
+This parenting practice aims to send the message that all feelings are okay, even the worst ones.
+END_ANSWER
+What is Emotion Coaching?
 END_QUESTION
